4d9ba95758a9ec9a0a91256ed66f636fc1e592eb
hulyy <2117124482@qq.com>
2025-04-01 21:10:58 +0800

    修改main

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@
 # @Author  : hu_ling_yi
 # @File    : main.py.py
 # @Time    : 2025/3/15 10:11
+import os
+
 from app.remindApp import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)), debug=True)
 
     # print(remindApp.config['SQLALCHEMY_DATABASE_URI'])
+    # (debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80))
