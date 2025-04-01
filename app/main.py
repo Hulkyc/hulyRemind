a889db9de22c62ca25205cc4d8629677cfda0236
hulyy <2117124482@qq.com>
2025-04-01 22:07:11 +0800

    添加接口

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.remindApp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)), debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
     # print(remindApp.config['SQLALCHEMY_DATABASE_URI'])
     # (debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80))
